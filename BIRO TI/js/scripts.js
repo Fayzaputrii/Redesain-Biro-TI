f2c22dc969ab41ec511535620dde3e5794d0ad02
fayza <fayzaputri64@gmail.com>
2024-07-24 11:49:05 +0700

    semuanya n pup up

--- BIRO TI/js/scripts.js
+++ BIRO TI/js/scripts.js
@@ -175,3 +175,13 @@ countBeda.forEach(element => {
     });
 
 });
+
+let currentSlide = 0;
+
+function moveSlide(direction) {
+  const slides = document.querySelectorAll('.carousel-slide');
+  const totalSlides = slides.length;
+  currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+  const carousel = document.querySelector('.carousel');
+  carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
